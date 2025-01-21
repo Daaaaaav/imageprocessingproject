e4dda94a36a92ee7bf4726faf22a06618bf77dcb
emiliagtg <emilsamsung596@gmail.com>
2025-01-21 13:12:02 +0700

    emilia

---
+++ def skeletonization.py
@@ -0,0 +1,14 @@
+def skeletonization(image):
+    gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    skeleton = np.zeros_like(binary)
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    while True:
+        open = cv2.morphologyEx(binary, cv2.MORPH_OPEN, element)
+        temp = cv2.subtract(binary, open)
+        eroded = cv2.erode(binary, element)
+        skeleton = cv2.bitwise_or(skeleton, temp)
+        binary = eroded.copy()
+        if cv2.countNonZero(binary) == 0:
+            break
+    return Image.fromarray(skeleton)
