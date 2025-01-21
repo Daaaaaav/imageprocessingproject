c2ff72d84d936d6b1c19d96113d85ed86eac84ac
emiliagtg <emilsamsung596@gmail.com>
2025-01-21 13:28:12 +0700

    Emilia's Contribution

--- gamma_correction.py
+++ gamma_correction.py
@@ -2,3 +2,4 @@ def gamma_correction(image, gamma=1.0):
     inv_gamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in range(256)]).astype("uint8")
     return Image.fromarray(cv2.LUT(np.array(image), table))
+  
